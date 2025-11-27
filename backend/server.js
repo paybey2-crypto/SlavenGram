fb0e239ce2321258f89d7e673cac6abbfe0a1022
transfer-app <paybey2@gmail.com>
2025-11-27 22:23:20 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/posts", postRoutes);
+
+app.get("/", (req, res) => res.send("SlavenGram backend radi!"));
+
+app.listen(3000, () => console.log("Server running on 3000"));
 
