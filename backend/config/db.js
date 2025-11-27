05c56a6a6bd689b556a06c1308948209280ea7d7
transfer-app <paybey2@gmail.com>
2025-11-27 22:37:12 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
 export const db = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // potrebno za Render
 });
 
